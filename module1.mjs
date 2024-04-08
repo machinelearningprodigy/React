b997f94778dd9fe182532e2c0d92f056e517f4e0
machinelearningprodigy <avaantivirus2021@gmail.com>
2024-04-08 06:43:31 +0530

    mjs files

---
+++ module1.mjs
@@ -0,0 +1,12 @@
+const a = "Natasha";
+const b = "Simran";
+const c = "Daniel";
+const d = "Roger";
+
+// these are default export
+export default c;
+
+// these are not default. These are called named export
+export {a};
+export {c};
+export {d};
